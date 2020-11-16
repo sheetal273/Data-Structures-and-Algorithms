e64977f3a1c8d5345be7a070309049a081048d4d
Sheetal Chhabra <sheetal.chhabra273@gmail.com>
2020-11-17 01:35:33 +0530

    Find median in a stream
    Given an input stream of N integers. The task is to insert these numbers into a new stream and find the median of the stream formed by each insertion of X to the new stream.
    
    Example 1:
    
    Input:
    N = 4
    X[] = 5,15,1,3
    Output:
    5
    10
    5
    4
    Explanation:Flow in stream : 5, 15, 1, 3 
    5 goes to stream --> median 5 (5) 
    15 goes to stream --> median 10 (5,15) 
    1 goes to stream --> median 5 (5,15,1) 
    3 goes to stream --> median 4 (5,15,1 3)

---
+++ GoogleInterviewQuestions/MedianInStream.java
@@ -0,0 +1,14 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MedianInStreamOfIntegers {
+
+
+    static PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+    static PriorityQueue<Integer> min = new PriorityQueue<>();
+    
+    // Check the full code here: https://www.patreon.com/posts/43972332
+    
+    
+    }
+  }
