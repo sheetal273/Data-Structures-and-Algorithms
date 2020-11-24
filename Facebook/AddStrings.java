72a01f2a1563e674dd3484d0f7694a629b86ea95
Sheetal Chhabra <sheetal.chhabra273@gmail.com>
2020-11-25 01:10:40 +0530

    Update AddStrings.java

--- Facebook/AddStrings.java
+++ Facebook/AddStrings.java
@@ -1,53 +1,38 @@
 class Solution {
     public String addStrings(String num1, String num2) {
         		 
- int i = num1.length()-1;
+ 
+		 int i = num1.length()-1;
 		 int j = num2.length()-1;
 		 
 		 int carry = 0;
-		 String result = "";
-		 while(i >= 0 && j >= 0) {
+		 StringBuilder result = new StringBuilder();
+		 while(i >= 0 || j >= 0) {
 			 
-			 int sum = carry + (num1.charAt(i)- '0' + num2.charAt(j) -'0');
-			 	 
-			 if(sum >= 10) {
+			 int sum = carry;
+			 if(i>=0) {
+				  sum += (num1.charAt(i--)-'0');
 				 
-				 carry = sum / 10;
-			 }else{
-				 
-				 carry = 0;
 			 }
-			 
-			 result = result + sum%10;
-			 
-			 i--;
-			 j--;	
-		 }
-		 
-		 
-		 while(i >= 0) {
-			 int sum = carry + (num1.charAt(i) - '0');
-			 result = result + sum %10;
-			 carry = sum /10;
-			 
-			 i --;
-		 }
-		 
-		 while(j >= 0) {
-			 System.out.println(" in j loop " + j  + " result "+ result);
+				 
+			 if(j>=0) {
+				  sum += (num2.charAt(j--)-'0');
+				 
+			 }
+
+			result.append(sum%10);
+			carry = sum / 10;
 			
-			 int sum = (carry + (num2.charAt(j) - '0'));
-			 carry = sum /10;
-			 
-			 result = result + sum%10;
-			 j --;
+				
 		 }
+		 
+	
+
 		 if(carry != 0) {
-			 result = result + carry;
+			 result.append(carry);
 		 }
 		
-		 StringBuilder str = new StringBuilder(result);
-		 return str.reverse().toString();
+		 return result.reverse().toString();
     }
     
     
