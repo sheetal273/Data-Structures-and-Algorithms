44e3a883601c766fe90b383af79750256ad4fd23
Sheetal Chhabra <sheetal.chhabra273@gmail.com>
2020-11-30 21:43:51 +0530

    Currency Exchange
    Example 1
    denominations = [5, 10, 25, 100, 200]
    targetMoney = 94
    output = false
    Every denomination is a multiple of 5, so you can't receive exactly 94 units of money in this currency.
    Example 2
    denominations = [4, 17, 29]
    targetMoney = 75
    output = true
    You can make 75 units with the denominations [17, 29, 29].

---
+++ Facebook/CurrencyExchange.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class CurrencyExchange {
+
+	public static void main(String args[]) {
+	
+		int[] denominations = {5, 10, 25, 100, 200};
+		System.out.println(canGetExactChange(75, denominations));
+	}
+	
+	public static boolean canGetExactChange(int targetMoney, int[] denominations) {
+	    // Write your code here
+	
+		return canGetExactChangeHelper(targetMoney, denominations, targetMoney, false);
+		
+	  }
+	
+	
+	public static boolean canGetExactChangeHelper(int targetMoney, int[] denominations, int remaining, boolean result) {
+	    // Write your code here
+	    
+		if(remaining == 0) {
+			System.out.println("*********** found **********");
+			
+			return true;
+		}
+		
+		if(remaining > targetMoney || remaining < 0) {
+			return false;
+		}
+
+		for(int i=0;i< denominations.length;i++) {
+
+			boolean ans = canGetExactChangeHelper(targetMoney, denominations, remaining - denominations[i], false, map, i, denominations[i]);
+			 result =  result || ans;
+			}
+		
+		return result;
+	  }
+}
